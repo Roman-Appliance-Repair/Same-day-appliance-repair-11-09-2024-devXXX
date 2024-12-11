bd66cd4199ecdec92bef4ec4097793d6973f8a36
null <null>
2024-12-11 12:25:34 -0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 
@@ -8,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://samedayappliance.repair",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), sitemap()],
 
 });
 
